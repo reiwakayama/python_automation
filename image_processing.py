efbf8ec7434c75b6e7a7549390c07ebac6cbc9a8
pink157cm <96822858+pink157cm@users.noreply.github.com>
2024-06-05 16:10:31 +0800

    Convert heic to png and remove exif metadata

--- image_processing.py
+++ image_processing.py
@@ -1,4 +1,4 @@
-import os 
+import os
 from PIL import Image
 import pillow_heif
 
@@ -10,6 +10,7 @@ def convert_heic_to_png(input_folder, output_folder):
     converted_files = 0
     exif_removed_files = 0
 
+    # Convert HEIC files to PNG
     for filename in os.listdir(input_folder):
         if filename.endswith('.heic'):
             input_path = os.path.join(input_folder, filename)
@@ -28,13 +29,22 @@ def convert_heic_to_png(input_folder, output_folder):
             # Delete the original heic file
             os.remove(input_path)
 
-    # Print the total number of files converted
-    print(f"Converted {converted_files} files to png")
+    # Strip EXIF metadata from existing image files in the output folder
+    for filename in os.listdir(output_folder):
+        if filename.endswith(('.png', '.jpg', '.jpeg')):
+            output_path = os.path.join(output_folder, filename)
+            with Image.open(output_path) as img:
+                # Remove exif metadata
+                data = list(img.getdata())
+                img_without_exif = Image.new(img.mode, img.size)
+                img_without_exif.putdata(data)
 
-    # Print the total number of files with EXIF metadata removed
+                img_without_exif.save(output_path, os.path.splitext(filename)[1][1:].upper())
+                exif_removed_files += 1
+
+    print(f"Converted {converted_files} files to png")
     print(f"Removed exif metadata from {exif_removed_files} files")
 
-# Example usage
-input_folder = "C:/Users/your/file/path/here/1"
-output_folder = "C:/Users/your/file/path/here/2"
+input_folder = "C:/Users/your/folder/path/1"
+output_folder = "C:/Users/your/folder/path/2"
 convert_heic_to_png(input_folder, output_folder)
